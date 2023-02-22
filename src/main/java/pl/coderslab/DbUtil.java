9d5c6bf90d9bd82b37073f1853f1dc6927597c1f
benzoesan <117587746+benzoesan@users.noreply.github.com>
2023-02-22 14:53:25 +0100

    tables and connections

---
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -0,0 +1,24 @@
+package pl.coderslab;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/exam2?useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "coderslab";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
+
+    public static void execute(Connection conn, String query) {
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,25 @@
 package pl.coderslab;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String QUERY1 = "CREATE DATABASE workshop2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;\n";
+    private static final String QUERY2 = "CREATE TABLE users (\n" +
+            "    id INT(11) NOT NULL AUTO_INCREMENT,\n" +
+            "    username varchar(255) DEFAULT NULL,\n" +
+            "    email varchar(255) UNIQUE DEFAULT NULL,\n" +
+            "    password varchar(255) DEFAULT NULL,\n" +
+            "    PRIMARY KEY (id)\n" +
+            ")";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection con = DbUtil.getConnection()){
+            DbUtil.execute(con,QUERY1);
+            DbUtil.execute(con,QUERY2);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
